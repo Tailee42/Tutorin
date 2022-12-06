9b85555046b46f8ec1b2dd5d206b45a879409257
Marie <brierremarie@gmail.com>
2022-12-06 18:27:51 +0100

    fix IresponsableServices

--- Tutorin/Services/IResponsableServices.cs
+++ Tutorin/Services/IResponsableServices.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using Tutorin.Models;
 
 namespace Tutorin.Services
 {
     public interface IResponsableServices : IDisposable
     {
-
+        int CreerResponsable(int utilisateurId, List<Abonnement> abonnements);
+        void ModifierResponsable(int id, int utilisateurId, List<Abonnement> abonnements);
     }
 }
