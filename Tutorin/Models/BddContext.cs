793b730e2087a97a52f9ddd2bdbfe98c4817bd86
Tailee42 <tailee05@hotmail.fr>
2022-12-06 22:09:01 +0100

    initialisation de quelques élèves

--- Tutorin/Models/BddContext.cs
+++ Tutorin/Models/BddContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace Tutorin.Models
 {
@@ -16,6 +17,27 @@ namespace Tutorin.Models
         {
             Database.EnsureDeleted();
             Database.EnsureCreated();
+        }
+
+        public void InitializaDb()
+        {
+            this.Database.EnsureDeleted();
+            this.Database.EnsureCreated();
+
+            this.Utilisateurs.AddRange(
+                new Utilisateur { Id = 1, Nom = "Albrand", Prenom = "Pierre", Identifiant = "apierre", MotDePasse = "pierre" },
+                new Utilisateur { Id = 2, Nom = "Roux", Prenom = "Louis", Identifiant = "rlouis", MotDePasse = "louis" },
+                new Utilisateur { Id = 3, Nom = "Queyras", Prenom = "Antoine", Identifiant = "qantoine", MotDePasse = "antoine" },
+                new Utilisateur { Id = 4, Nom = "Dupond", Prenom = "Anne", Identifiant = "danne", MotDePasse = "anne" }
+                );
+
+            this.Eleves.AddRange(
+                new Eleve { Id = 1, DateNaissance = new System.DateTime(2013, 07, 14), Niveau = TypeNiveau.CM1, UtilisateurID = 2 },
+                new Eleve { Id = 2, DateNaissance = new System.DateTime(2011, 04, 01), Niveau = TypeNiveau.Sixieme, UtilisateurID = 4 }
+
+                );
+
+            this.SaveChanges();
         }
     }
 }
