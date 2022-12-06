9bea3252c493ba6d76a4f67eb65faea688ce0c31
OUAFO KENGNE <kengneouafo@yahoo.com>
2022-12-06 16:30:32 +0100

    creation de la classe enseignant et type de niveau

---
+++ Tutorin/Models/Enseignant.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tutorin.Models
+{
+    public class Enseignant
+    {
+        public int Id { get; set; }
+        public string Matière { get; set; }
+        public TypeNiveau Niveaux { get; set; }
+        public int UtilisateurId { get; set; }
+        public Utilisateur Utilisateur { get; set; }
+
+    }
+}
---
+++ Tutorin/Models/TypeNiveau.cs
@@ -0,0 +1,7 @@
+﻿namespace Tutorin.Models
+{
+    public enum TypeNiveau
+    {
+        CP = 0, CE1 = 1, CE2, CM1, CM2, Sixieme, Cinquieme, Quatrieme, Troisieme, Seconde, Premiere, Terminale
+    }
+}
