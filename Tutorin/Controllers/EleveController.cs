254e5710dce3221bec82ffedc627edf934493a9d
Tailee42 <tailee05@hotmail.fr>
2022-12-06 19:02:49 +0100

    ajout de la vue liste des élèves et du controller

---
+++ Tutorin/Controllers/EleveController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tutorin.Services;
+using Tutorin.ViewModels;
+using System.Collections.Generic;
+using Tutorin.Models;
+
+namespace Tutorin.Controllers
+{
+    public class EleveController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Eleve> listeEleves = new List<Eleve>();
+            EleveViewModel evm;
+
+            using (EleveServices es = new EleveServices())
+            {
+                evm = new EleveViewModel()
+                {
+                    ListeEleves = es.ObtientTousLesEleves()
+
+                };
+            };
+            
+            return View("ListeEleves", evm);
+        }
+    }
+}
