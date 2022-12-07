03ba41d643aa8f35023cf61ce2b3cccd80575d0e
Tailee42 <tailee05@hotmail.fr>
2022-12-07 15:32:46 +0100

    Changement utilisateurID en utilisateurId pour l'élève

--- Tutorin/Services/EleveServices.cs
+++ Tutorin/Services/EleveServices.cs
@@ -17,7 +17,7 @@ namespace Tutorin.Services
 
         public int CreerEleve(DateTime dateNaissance, TypeNiveau niveau, int utilisateurID)
         {
-            Eleve eleve = new Eleve() { DateNaissance = dateNaissance, Niveau = niveau, UtilisateurID = utilisateurID};
+            Eleve eleve = new Eleve() { DateNaissance = dateNaissance, Niveau = niveau, UtilisateurId = utilisateurID};
             _bddContext.Eleves.Add(eleve);
             _bddContext.SaveChanges();
 
@@ -42,7 +42,7 @@ namespace Tutorin.Services
         public void ModifierEleve(int id, string nom, string prenom, string identifiant, string motDePasse, DateTime dateNaissance, TypeNiveau niveau)
         {
             Eleve eleve = _bddContext.Eleves.Find(id);
-            eleve.Utilisateur = _bddContext.Utilisateurs.Find(eleve.UtilisateurID);
+            eleve.Utilisateur = _bddContext.Utilisateurs.Find(eleve.UtilisateurId);
             if (eleve != null)
             {
                 eleve.Utilisateur.Nom = nom;
@@ -66,7 +66,7 @@ namespace Tutorin.Services
             List <Eleve> listeEleves = _bddContext.Eleves.ToList();
             foreach (Eleve eleve in listeEleves)
             {
-                eleve.Utilisateur = _bddContext.Utilisateurs.Find(eleve.UtilisateurID);
+                eleve.Utilisateur = _bddContext.Utilisateurs.Find(eleve.UtilisateurId);
             }
 
             return listeEleves;
