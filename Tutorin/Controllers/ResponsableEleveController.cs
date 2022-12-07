fd37ceae23ce37a945c5ffc568f5cc4b53ef3e2b
Marie <brierremarie@gmail.com>
2022-12-07 11:43:53 +0100

    create form to add a new responsableEleve

---
+++ Tutorin/Controllers/ResponsableEleveController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tutorin.Controllers
+{
+    public class ResponsableEleveController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
