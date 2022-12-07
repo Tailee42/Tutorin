3d91780bfd4d5b64cd46b82000564c290d9579ff
Tailee42 <tailee05@hotmail.fr>
2022-12-07 17:09:33 +0100

    création PrestationService

--- Tutorin/Models/Prestation.cs
+++ Tutorin/Models/Prestation.cs
@@ -1,17 +1,24 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Tutorin.Models
 {
     public class Prestation
     {
         public int Id { get; set; }
+        [Required]
         public TypeNiveau Niveau { get; set; }
+        [Required]
         public DateTime DateDebut { get; set; }
+        [Required]
         public DateTime DateFin { get; set; }
+        [Required]
         public TypePrestation TypePrestation { get; set; } 
         public string Ville { get; set; }
-        public double Prix { get; set; }
-        public TypeMatiere TypeMatiere { get; set; }
+        public float Prix { get; set; }
+        [Required]
+        public TypeMatiere Matiere { get; set; }
+        [Required]
         public Boolean Presentiel { get; set; }
         public EtatPrestation EtatPrestation { get; set; }
         public string LienVisio { get; set; }
---
+++ Tutorin/Services/IPrestationServices.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Tutorin.Models;
+
+namespace Tutorin.Services
+{
+    public interface IPrestationServices : IDisposable
+    {
+        int CreerPrestation(TypeNiveau niveau, TypeMatiere matiere, DateTime dateDebut, DateTime dateFin, TypePrestation prestation, 
+            string Ville, float prix, Boolean Presentiel, string lienVisio, int enseignantId );
+        void ModifierPrestation(Prestation prestation);
+        void SupprimerPrestation(int id);
+        List<Prestation> ObtientTousLesPrestations();
+    }
+}
