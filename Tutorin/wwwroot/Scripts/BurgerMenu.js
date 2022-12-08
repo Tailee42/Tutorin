1fe08f6cf9603f68cb6f62a538192d4874575dab
Tailee42 <tailee05@hotmail.fr>
2022-12-08 23:26:36 +0100

    burger menu responsive

---
+++ Tutorin/wwwroot/Scripts/BurgerMenu.js
@@ -0,0 +1,15 @@
+﻿
+document.getElementById("navbar-mobile-list").innerHTML = document.getElementById("navbar-desktop").innerHTML;
+
+/* Commande du burgerMenu*/
+const icone = document.querySelector('.navbar-mobile i');
+console.log(icone);
+
+const modal = document.querySelector('.modal');
+console.log(modal)
+
+icone.addEventListener('click', function () {
+    console.log("icone cliqué");
+    modal.classList.toggle('change-modal');
+    icone.classList.toggle('fa-times');
+});
