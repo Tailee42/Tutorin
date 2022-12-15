b49b4795a6b6b89b40d03f78d11045e15cf0820a
Marie <brierremarie@gmail.com>
2022-12-15 12:04:19 +0100

    methodes enum  pour corriger les noms matieres, niveaux et états = ok

--- Tutorin/Models/EtatPrestation.cs
+++ Tutorin/Models/EtatPrestation.cs
@@ -4,4 +4,33 @@
     {
         A_affecter, Enseignants_inscrits, Payee_par_responsable_eleve, Realisée, Enseigants_payes, Attestation_a_faire, Facturee, Annulee
     }
+
+    public static class EtatPrestationExtensions
+    {
+        public static string NomEtat(EtatPrestation prestation)
+        {
+            switch ((int)prestation)
+            {
+                case 0:
+                    return "A affecter";
+                case 1:
+                    return "Enseignants inscrits";
+                case 2:
+                    return "Payée par le responsable élève";
+                case 3:
+                    return "Réalisée";
+                case 4:
+                    return "Enseignants payés";
+                case 5:
+                    return "Attestation à faire";
+                case 6:
+                    return "Facturée";
+                case 7:
+                    return "Annulée";
+            }
+            return null;
+        }
+
+
+    }
 }
--- Tutorin/Models/TypeMatiere.cs
+++ Tutorin/Models/TypeMatiere.cs
@@ -4,4 +4,31 @@
     {
        Anglais, Français, Mathematiques, Physique, Chimie, Histoire, Geographie
     }
+
+    public static class TypeMatiereExtensions
+    {
+        public static string NomMatiere(TypeMatiere matiere)
+        {
+            switch ((int)matiere)
+            {
+                case 0:
+                    return "Anglais";
+                case 1:
+                    return "Français";
+                case 2:
+                    return "Mathématiques";
+                case 3:
+                    return "Physique";
+                case 4:
+                    return "Chimie";
+                case 5:
+                    return "Histoire";
+                case 6:
+                    return "Géographie";
+            }
+            return null;
+        }
+
+
+    }
 }
