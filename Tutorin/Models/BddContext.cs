233c4957bbf9df7bf710ed84534a2f799ec5ef23
Tailee42 <tailee05@hotmail.fr>
2022-12-20 12:01:06 +0100

    ajout des photos dans la base de données

--- Tutorin/Models/BddContext.cs
+++ Tutorin/Models/BddContext.cs
@@ -262,10 +262,10 @@ namespace Tutorin.Models
 
 
             this.Enseignants.AddRange(
-                new Enseignant{ Id = 1, Matiere = TypeMatiere.Mathematiques, Niveaux = TypeNiveau.CM1, UtilisateurId = 5 , ImagePath = "/images/enseignant.jpg" },
-                new Enseignant { Id = 2, Matiere = TypeMatiere.Physique, Niveaux = TypeNiveau.CM1, UtilisateurId = 6, ImagePath = "/images/enseignant.jpg" },
-                new Enseignant { Id = 3, Matiere = TypeMatiere.Geographie, Niveaux = TypeNiveau.Troisieme, UtilisateurId = 8, ImagePath = "/images/enseignant.jpg" },
-                new Enseignant { Id = 4, Matiere = TypeMatiere.Français, Niveaux = TypeNiveau.CP, UtilisateurId = 3, ImagePath = "/images/enseignant.jpg" }
+                new Enseignant{ Id = 1, Matiere = TypeMatiere.Mathematiques, Niveaux = TypeNiveau.CM1, UtilisateurId = 5 , ImagePath = "/images/Bonheur.jpg" },
+                new Enseignant { Id = 2, Matiere = TypeMatiere.Physique, Niveaux = TypeNiveau.CM1, UtilisateurId = 6, ImagePath = "/images/Louise.jpg" },
+                new Enseignant { Id = 3, Matiere = TypeMatiere.Geographie, Niveaux = TypeNiveau.Troisieme, UtilisateurId = 8, ImagePath = "/images/Pachere.jpg" },
+                new Enseignant { Id = 4, Matiere = TypeMatiere.Français, Niveaux = TypeNiveau.CP, UtilisateurId = 3, ImagePath = "/images/Queyras.jpg" }
                 );
 
             this.ContenusPedagogiques.AddRange(
